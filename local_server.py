20e46e6ccedb54be2ff75088028de90f68e4428c
SiD4422 <sid4422@gmail.com>
2026-07-28 18:50:08 +0530

    Fix quote escaping in GWT cache files for VCVS/VCCS/CCVS/CCCS buttons and add no-cache headers to local server

--- local_server.py
+++ local_server.py
@@ -23,6 +23,12 @@ class NetlifyHandler(http.server.SimpleHTTPRequestHandler):
                 
         return translated_path
 
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
     def do_POST(self):
         if self.path == '/dump':
             content_length = int(self.headers['Content-Length'])
